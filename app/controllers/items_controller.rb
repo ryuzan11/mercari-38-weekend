05e7c3d80ec0976835bfd5d0a3ce5622dc8248e4
aqua25 <h.aqua25@gmail.com>
2019-02-17 17:04:29 +0900

    Changed devise before&after views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
+
   def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
