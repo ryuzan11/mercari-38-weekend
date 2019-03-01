acd56d9c9248ba995d43d67cdb3e9bdac05914a9
増井啓之 <masuikeishi@masuikeijinoMacBook-puro.local>
2019-03-01 15:32:29 +0900

    Correct routing mistake

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "items#index"
   get 'items/show', to: 'items#show'
   get 'items/detail', to: 'items#detail'
-  resources :items except: :show
+  resources :items, except: :show
 end
