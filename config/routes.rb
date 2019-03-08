b1a33070afe3db2373a7e6006f6158f7e101949e
Yuta Osaka <yusoac@gmail.com>
2019-03-08 20:08:58 +0900

    Define creditcard routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "items#index"
   get 'items/show', to: 'items#show'
   get 'items/detail', to: 'items#detail'
+  get 'creditcards/new', to: 'creditcards#new'
   resources :items, except: :show
   resources :users, only: [:show]
+  resources :creditcard, only: [:new]
 
 end
