d408fb1cb5484628066630768365fecc42c5237b
aqua25 <h.aqua25@gmail.com>
2019-03-08 21:06:31 +0900

    Conflict delete.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,30 +3,10 @@ class ItemsController < ApplicationController
   def index
   end
 
-<<<<<<< HEAD
-  def new
-  end
-
-  def edit
-  end
-
-  def create
-  end
-
-  def show
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-=======
   def show
   end
 
   def detail
   end
 
->>>>>>> keishi-masui/master
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-<<<<<<< HEAD
-  resources :items
-  resources :signup, only: [:index]
-=======
   get 'items/show', to: 'items#show'
   get 'items/detail', to: 'items#detail'
   resources :items, except: :show
   resources :users, only: [:show]
-
->>>>>>> keishi-masui/master
 end
