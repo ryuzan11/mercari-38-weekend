f1fea28f4abde2843a60e528cb3171745d4a7f57
aqua25 <h.aqua25@gmail.com>
2019-03-09 15:09:21 +0900

    Write user-table columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190217052108) do
+ActiveRecord::Schema.define(version: 20190309060402) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20190217052108) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "nickname",                            null: false
+    t.string   "first_name",                          null: false
+    t.string   "second_name",                         null: false
+    t.string   "first_name_kana",                     null: false
+    t.string   "second_name_kana",                    null: false
+    t.integer  "birth_year",                          null: false
+    t.integer  "birth_month",                         null: false
+    t.integer  "birth_day",                           null: false
+    t.string   "phone_number",                        null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
