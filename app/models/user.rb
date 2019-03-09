589a6a457365a87832024da4a03f0b318cf285ef
Takashi Morise <typwx190@yahoo.co.jp>
2019-03-09 21:02:07 +0900

    Create item model and items table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :address
-  has_many :user_evalutions
-  has_many :evaluations, througth: user_evaluations
   has_many :credits
   has_many :SNS_authentications
   has_many :favorites
