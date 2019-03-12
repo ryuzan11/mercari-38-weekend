bfdbf95a124324ef0fab3af07353aff3501fb726
増井啓之 <masuikeishi@masuikeijinoMacBook-puro.local>
2019-03-12 22:51:54 +0900

    Routing   edit  routes.rb and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # item関連
   root "items#index"
   resources :items
   resources :signup, only: [:index]
   get 'items/show', to: 'items#show'
   get 'items/detail', to: 'items#detail'
   resources :items, except: :show
+
+  # user関連
+  get 'users/register', to: 'users#register'
   resources :users, only: [:show, :edit]
+
+
 
 end
