6cbcaf06e23fe0ccb61812b82ff9d7d29089c537
増井啓之 <masuikeishi@masuikeijinoMacBook-puro.local>
2019-03-16 00:03:59 +0900

    Edit route & controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-  resources :items
-  resources :signup, only: [:index]
+  get 'items/confirm_buy', to: 'items#confirm_buy'
   get 'items/show', to: 'items#show'
   get 'items/detail', to: 'items#detail'
+  resources :items
+  resources :signup, only: [:index]
+
   resources :items, except: :show
+
+
   resources :users, only: [:show, :edit]
 
 end
