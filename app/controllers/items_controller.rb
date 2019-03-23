15fc1477d9de8a707655d4f522fd2afa48259720
増井啓之 <masuikeishi@masuikeijinoMacBook-puro.local>
2019-03-23 11:45:49 +0900

    (controller) edit create action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,12 +4,17 @@ class ItemsController < ApplicationController
   end
 
   def new
-  end
-
-  def edit
+    @item = Item.new
+    # @categories = Category.all
   end
 
   def create
+    @item = Item.create(item_params)
+
+     render action: :new
+  end
+
+  def edit
   end
 
   def show
@@ -28,6 +33,12 @@ class ItemsController < ApplicationController
   end
 
   def confirm_buy
+  end
+
+  private
+
+  def  item_params
+    params.require(:item).permit(:name, :category_id, :price, :info, :condition, :delivery_fee, :departure_area, :departure_day).merge(user_id: current_user.id,status: 1)
   end
 
 end
