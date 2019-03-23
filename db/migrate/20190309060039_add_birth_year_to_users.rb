277bd93d41ded8a3f5a7ad59b75d4ebb1f12cc98
増井啓之 <masuikeishi@masuikeijinoMacBook-puro.local>
2019-03-23 14:32:38 +0900

    Migration/file

--- db/migrate/20190309060039_add_birth_year_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddBirthYearToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :birth_year, :integer, null: false
-  end
-end
--- db/migrate/20190309060236_add_birth_day_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddBirthDayToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :birth_day, :integer, null: false
-  end
-end
