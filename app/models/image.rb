df379f28454a30f90cde789f26d0e1d1a129865d
keishi-masui <43719853+keishi-masui@users.noreply.github.com>
2019-03-23 17:17:35 +0900

    Revert "Fixed model associations"

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,2 @@
 class Image < ApplicationRecord
-  belongs_to :item
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   # belongs_to :brand
-  has_many :images
+  # has_many :images
 
   # has_many :comments
 
