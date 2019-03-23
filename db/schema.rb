d4db7befdec47d405a4532ada17ecbf1d631d8d3
増井啓之 <masuikeishi@masuikeijinoMacBook-puro.local>
2019-03-23 12:29:27 +0900

    Revert "db Migrating"
    This reverts commit 0cc59e88b07bb5afc0f495cc62dee43f1736b792.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190317075109) do
+ActiveRecord::Schema.define(version: 20190317065725) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -40,16 +40,6 @@ ActiveRecord::Schema.define(version: 20190317075109) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "favorite"
-    t.integer  "item_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_favorites_on_item_id", using: :btree
-    t.index ["user_id"], name: "index_favorites_on_user_id", using: :btree
-  end
-
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "image",      null: false
     t.integer  "item_id"
@@ -59,22 +49,22 @@ ActiveRecord::Schema.define(version: 20190317075109) do
   end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                          default: ""
-    t.integer  "price"
-    t.text     "condition",       limit: 65535
-    t.string   "status",                        default: ""
-    t.text     "info",            limit: 65535
-    t.string   "size",                          default: ""
-    t.integer  "delivery_fee"
-    t.string   "delivery_method",               default: ""
-    t.string   "departure_area",                default: ""
-    t.string   "departure_day",                 default: ""
+    t.string   "name",                          null: false
+    t.integer  "price",                         null: false
+    t.text     "condition",       limit: 65535, null: false
+    t.string   "status",                        null: false
+    t.text     "info",            limit: 65535, null: false
+    t.string   "size",                          null: false
+    t.integer  "delivery_fee",                  null: false
+    t.string   "delivery_method",               null: false
+    t.string   "departure_area",                null: false
+    t.string   "departure_day",                 null: false
     t.integer  "buyer_id"
-    t.integer  "user_id"
-    t.integer  "category_id"
-    t.integer  "brand_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer  "user_id",                       null: false
+    t.integer  "category_id",                   null: false
+    t.integer  "brand_id",                      null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.index ["brand_id"], name: "index_items_on_brand_id", using: :btree
     t.index ["category_id"], name: "index_items_on_category_id", using: :btree
     t.index ["user_id"], name: "index_items_on_user_id", using: :btree
@@ -93,14 +83,16 @@ ActiveRecord::Schema.define(version: 20190317075109) do
     t.string   "second_name",                         null: false
     t.string   "first_name_kana",                     null: false
     t.string   "second_name_kana",                    null: false
+    t.integer  "birth_year",                          null: false
+    t.integer  "birth_month",                         null: false
+    t.integer  "birth_day",                           null: false
+    t.string   "phone_number",                        null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "comments", "items"
   add_foreign_key "comments", "users"
-  add_foreign_key "favorites", "items"
-  add_foreign_key "favorites", "users"
   add_foreign_key "images", "items"
   add_foreign_key "items", "brands"
   add_foreign_key "items", "categories"
