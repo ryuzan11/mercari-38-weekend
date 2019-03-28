487a2d861af58044aa58158e46cc3956da431672
増井啓之 <masuikeishi@masuikeijinoMacBook-puro.local>
2019-03-28 12:28:25 +0900

    (model) item&brand_model  edit  for  brand-form

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,10 @@ class Item < ApplicationRecord
 
   belongs_to :user
   belongs_to :category
-  # belongs_to :brand
+  belongs_to :brand, optional: true
   has_many :images
   accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :brand
 
   # has_many :comments
 
