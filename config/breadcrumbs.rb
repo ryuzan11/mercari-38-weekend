065bfaff1c6445ed30070dc4df0642f62a6df1aa
Yuta Osaka <yusoac@gmail.com>
2019-04-16 16:26:37 +0900

    パンくずリストの設定ファイルを編集

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,5 +1,5 @@
 crumb :root do
-  link "Home", root_path
+  link "ホーム", root_path
 end
 
 crumb :show_items do |item|
@@ -7,8 +7,18 @@ crumb :show_items do |item|
   parent :root
 end
 
-crumb :show_users do |user|
-  link 'ユーザーページ', users_profile_path
+crumb :register_users do |user|
+  link '本人情報', users_profile_path
+  parent :root
+end
+
+crumb :profile_users do |user|
+  link 'プロフィール', users_profile_path
+  parent :root
+end
+
+crumb :mypage_users do |user|
+  link 'マイページ', users_profile_path
   parent :root
 end
 
